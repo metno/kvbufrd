f554c931d6f6ae4723d5d3c0a38b54c02ee7f828
Børge Moe <borge.moe@met.no>
2010-09-07 13:42:28 +0000

    Add methods to encode/decode base64.
    git-svn-id: https://svn.met.no/kvoss/kvbufferd/trunk@2001 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/base64.cc
+++ src/base64.cc
@@ -205,14 +205,48 @@ VERSION HISTORY:
 
 ******************************************************************* */
 
-#ifndef __MY_BASE64_H__
-#define __MY_BASE64_H__
 
-#include <string>
-#define B64_DEF_LINE_SIZE   72
-#define B64_MIN_LINE_SIZE    4
+#include <sstream>
+#include "base64.h"
 
+using namespace std;
 
+namespace {
+/*
+ ** Translation Table as described in RFC1113
+ */
+const char cb64[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+/*
+ ** Translation Table to decode (created by author)
+ */
+const char cd64[]="|$$$}rstuvwxyz{$$$$$$$>?@ABCDEFGHIJKLMNOPQRSTUVW$$$$$$XYZ[\\]^_`abcdefghijklmnopq";
+
+/*
+ ** encodeblock
+ **
+ ** encode 3 8-bit binary bytes as 4 '6-bit' characters
+ */
+void encodeblock( unsigned char in[3], unsigned char out[4], int len )
+{
+   out[0] = cb64[ in[0] >> 2 ];
+   out[1] = cb64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
+   out[2] = (unsigned char) (len > 1 ? cb64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
+   out[3] = (unsigned char) (len > 2 ? cb64[ in[2] & 0x3f ] : '=');
+}
+
+/*
+ ** decodeblock
+ **
+ ** decode 4 '6-bit' characters into 3 8-bit binary bytes
+ */
+void decodeblock( unsigned char in[4], unsigned char out[3] )
+{
+   out[ 0 ] = (unsigned char ) (in[0] << 2 | in[1] >> 4);
+   out[ 1 ] = (unsigned char ) (in[1] << 4 | in[2] >> 2);
+   out[ 2 ] = (unsigned char ) (((in[2] << 6) & 0xc0) | in[3]);
+}
+}
 
 /*
 ** encode
@@ -220,12 +254,84 @@ VERSION HISTORY:
 ** base64 encode a stream adding padding and line breaks as per spec.
 */
 void
-encode64( const char *source, int size,  std::string &dest_, int linesize=B64_DEF_LINE_SIZE );
+encode64( const char *source, int size,  std::string &dest_, int linesize )
+{
+    unsigned char in[3], out[4];
+    int i, len, blocksout = 0;
+    int iSource=0;
+    ostringstream dest;
+
+    while( iSource < size ) {
+        len = 0;
+        for( i = 0; i < 3; i++ ) {
+            if( iSource < size ) {
+               in[i] = (unsigned char) source[iSource++];
+               len++;
+            }else {
+                in[i] = 0;
+            }
+        }
+        if( len ) {
+            encodeblock( in, out, len );
+            for( i = 0; i < 4; i++ ) {
+               dest << out[i];
+            }
+            blocksout++;
+        }
+        if( blocksout >= (linesize/4) || iSource >= size ) {
+            if( blocksout ) {
+                dest << "\r\n";
+            }
+            blocksout = 0;
+        }
+    }
+
+    dest_ = dest.str();
+}
+
 
 /*
 ** decode
 **
 ** decode a base64 encoded stream discarding padding, line breaks and noise
 */
-void decode64( const std::string &source,  std::string &dest );
-#endif
+void
+decode64( const std::string &source /*infile*/,  std::string &dest /*outfile*/ )
+{
+    unsigned char in[4], out[3], v;
+    int i, len;
+    ostringstream oDest;
+    std::string::size_type iSource=0;
+    dest.erase();
+
+    while( iSource < source.size() ) {
+        for( len = 0, i = 0; i < 4 &&  iSource<source.size(); i++ ) {
+            v = 0;
+            while( iSource<source.size() && v == 0 ) {
+                v = (unsigned char) source[iSource++];
+                v = (unsigned char) ((v < 43 || v > 122) ? 0 : cd64[ v - 43 ]);
+                if( v ) {
+                    v = (unsigned char) ((v == '$') ? 0 : v - 61);
+                }
+            }
+            if( iSource < source.size() ) {
+                len++;
+                if( v ) {
+                    in[ i ] = (unsigned char) (v - 1);
+                }
+            }
+            else {
+                in[i] = 0;
+            }
+        }
+        if( len ) {
+            decodeblock( in, out );
+            for( i = 0; i < len - 1; i++ ) {
+                oDest << out[i];
+            }
+        }
+    }
+
+    dest = oDest.str();
+}
+
