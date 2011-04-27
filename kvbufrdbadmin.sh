2c8f3be37705807ea532be4e3b7389cebde746e1
Børge Moe <borge.moe@met.no>
2011-04-27 07:51:59 +0000

    Fix the clean script and cronjob.
    git-svn-id: https://svn.met.no/kvoss/kvbufrd/trunk@2873 422d51e2-5941-41ac-8e65-d5b5931239be

--- kvbufrdbadmin.sh
+++ kvbufrdbadmin.sh
@@ -3,9 +3,10 @@
 SQL=sqlite3
 
 LOCAL_CONF_FILE=`KVCONFIG --sysconfdir`/kvalobs/bufrdbclean.sql
-SQLDIR=`KVCONFIG --datadir`/kvbufrd/
+SQLDIR=`KVCONFIG --datadir`/kvbufrd
 LOGDIR=`KVCONFIG --logdir`
 DBFILE=`KVCONFIG --localstatedir`/lib/kvbufrd/kvbufr.sqlite
+SQLCLEAN=$SQLDIR/cleanbufrdb.sql
 
 if [ -f $LOCAL_CONF_FILE ]; then
 	DBFILE=$LOCAL_CONF_FILE
@@ -14,10 +15,21 @@ fi
 DAY=`date '+%d'`
 LOG=$LOGDIR/kvbufr/kvbufrdbadmin-$DAY.log
 
+die()
+{
+   tstamp=`date`
+   echo "$tstamp - Do NOT clean db on this machine." > $LOG
+   exit 0
+}
+
+(test -f "$HOME/etc/KVALOBS_TEST") || ( /usr/local/sbin/alias_list | /bin/grep -q kvalobs ) || die
+
+
 echo -n "Start: " > $LOG
 date >> $LOG
+echo "Clean script: $SQLCLEAN" >> $LOG
 echo "--------------------------------------------------" >> $LOG 
-$SQL $DBFILE  < $SQLDIR/cleanbufrdb.sql >> $LOG  2>&1
+$SQL $DBFILE  < $SQLCLEAN >> $LOG  2>&1
 echo "--------------------------------------------------" >> $LOG 
 echo -n "Stop: " >> $LOG
 date >> $LOG
