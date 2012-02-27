54cc58768cf1b946db8391fe98b894e52d8058b6
Børge Moe <borge.moe@met.no>
2012-02-27 10:52:44 +0000

    Validate that the setup is valid.
    git-svn-id: https://svn.met.no/kvoss/kvbufrd/trunk@3362 422d51e2-5941-41ac-8e65-d5b5931239be

--- kvbufrdbadmin.sh
+++ kvbufrdbadmin.sh
@@ -11,13 +11,12 @@ DBFILE=`$KVCONFIG --localstatedir`/lib/kvbufrd/kvbufr.sqlite
 SQLCLEAN=$SQLDIR/cleanbufrdb.sql
 LIBDIR=`$KVCONFIG --pkglibdir`
 
-if [ ! -f "$LIBDIR/tool_funcs.sh" ]; then
-	echo "Cant load: $LIBDIR/tool_funcs.sh"
-	exit 1
-fi
-
-. $LIBDIR/tool_funcs.sh
-
+#if [ ! -f "$LIBDIR/tool_funcs.sh" ]; then
+#	echo "Cant load: $LIBDIR/tool_funcs.sh"
+#	exit 1
+#fi
+#
+#
 #Exit if the machines do NOT hold the ipalias or is an test machine.
 #ipalias_status > /dev/null || exit 0 
 
@@ -25,14 +24,32 @@ if [ -f $LOCAL_CONF_FILE ]; then
 	SQLCLEAN=$LOCAL_CONF_FILE
 fi
 
+
 DAY=`date '+%d'`
 LOG=$LOGDIR/kvbufr/kvbufrdbadmin-$DAY.log
 
+[ -f "$LOG" ] && rm -rf "$LOG" 
+
+if [ ! -f "$DBFILE" ]; then
+    echo "No dbfile: $DBFILE" >> $LOG  
+    exit 0
+fi
+
+if [ ! -f "$SQLCLEAN" ]; then
+    echo "No sqlclean file: $SQLCLEAN" >> $LOG
+    exit 0
+fi 
+
+if ! which $SQL > /dev/null 2>&1; then
+    echo "Cant find the program <$SQL>."
+    exit 1
+fi
 
 
-echo -n "Start: " > $LOG
+echo -n "Start: " >> $LOG
 date >> $LOG
 echo "Clean script: $SQLCLEAN" >> $LOG
+echo "Sqlite db: $DBFILE" >> $LOG
 echo "--------------------------------------------------" >> $LOG 
 $SQL $DBFILE  < $SQLCLEAN >> $LOG  2>&1
 echo "--------------------------------------------------" >> $LOG 
